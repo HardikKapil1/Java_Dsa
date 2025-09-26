666ecafe243b41a4c3bcaae979dad6ae55f20ede
HardikKapil1 <kapilhardik11@gmail.com>
2025-09-26 20:33:10 +0530

    Add BasicDeclaration class for multidimensional array demonstration

---
+++ Multidimensional Array/BasicDeclaration.java
@@ -0,0 +1,15 @@
+public class BasicDeclaration {
+    public static void main(String[] args) {
+        int[][] grid= new int[3][3];
+        grid[0][0]=10;
+        grid[0][1]=20;
+        grid[0][2]=30;
+        grid[1][0]=10;
+        grid[1][0]=10;
+        grid[1][0]=10;
+        grid[0][0]=10;
+        grid[0][0]=10;
+        grid[0][0]=10;
+        System.out.println(grid[2][2]);
+    }
+}
